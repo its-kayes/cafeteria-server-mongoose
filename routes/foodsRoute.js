6c1cde443fa961ef07f97e4c62a1e9207a00e046
its-kayes <kayes.ek8@gmail.com>
2022-08-03 18:52:24 +0600

    Get API create for Foods Load

--- routes/foodsRoute.js
+++ routes/foodsRoute.js
@@ -12,11 +12,57 @@ router.get('/:id', async (req, res) => {
 })
 
 // get all foods
-router.get('/', async (req, res) => {
-    res.send({
-        status: "Foods GET API working"
-    })
+router.get("/", async (req, res) => {
+    try {
+        const data = await Foods.find({ desc: "Test Sea Fish" });
+        res.send({ data, message: 'Successfully loaded products', success: true });
+    } catch (error) {
+        res.status(500).send({ error: error, message: 'Server side error', success: false });
+    }
+
+    // await Foods.find({}, function (err, docs) {
+    //     if (!err) {
+    //         res.status(200).json({
+    //             result: docs
+    //         })
+    //     }
+    // });
+
+
+    // (Working)
+    // let data = await Foods.find({})
+    // res.send({data, status: "success"});
+
+
+
+    // res.send({
+    //     status: "Foods GET API working"
+    // })
 })
+
+
+// router.get("/", async (req, res) => {
+//     await Foods.find({ status: "true" })
+//         .select({
+//             _id: 0,
+//             __v: 0,
+//             date: 0,
+//         })
+//         .limit(2)
+//         .exec((err, data) => {
+//             if (err) {
+//                 res.status(500).json({
+//                     error: "There was a server side error!",
+//                 });
+//             } else {
+//                 res.status(200).json({
+//                     result: data,
+//                     message: "Success",
+//                 });
+//             }
+//         });
+// })
+
 
 // post data on foods
 // router.post('/', async (req, res) => {
@@ -36,20 +82,26 @@ router.get('/', async (req, res) => {
 // })
 
 
+
+// Single Data Post (Working)
 router.post("/", async (req, res) => {
     const newFoods = new Foods(req.body);
     await newFoods.save((err) => {
+        // await Foods(req.body).save((err) => {
         if (err) {
             res.status(500).json({
                 error: "Food part Server Side Error",
             });
         } else {
             res.status(200).json({
-                message: "Food Data insert!",
+                message: `${req.body.title} insert in to Database!`,
             });
         }
     });
 });
+
+
+
 
 
 module.exports = router;
